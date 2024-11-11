4bab6f9434bd176c923dd9c4dd4aa8733fe16fa4
Almazz <050606500373@stud.satbayev.university>
2024-11-11 18:07:02 +0500

    modul10

---
+++ LABORATORY/Program.cs
@@ -0,0 +1,33 @@
+﻿using LABORATORY;
+using Directory = System.IO.Directory;
+using File = System.IO.File;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        AudioSystem audioSystem = new AudioSystem();
+        VideoProjector videoProjector = new VideoProjector();
+        LightingSystem lightingSystem = new LightingSystem();
+
+        HomeTheaterFacade homeTheaterFacade = new HomeTheaterFacade(audioSystem, videoProjector, lightingSystem);
+        homeTheaterFacade.StartMovie();
+        homeTheaterFacade.StopMovie();
+        //////////////////////////////////////////////////////
+
+        Directory root = new Directory("Root");
+        File file1 = new File("File1.txt");
+        File file2 = new File("File2.txt");
+        
+        Directory subDir = new Directory("SubDir");
+        File subFile1 = new File("SubFile1.txt");
+
+        root.Add(file1);
+        root.Add(file2);
+        
+        subDir.Add(subFile);
+        root.Add(subDir);
+
+        root.Display(1);
+    }
+}
